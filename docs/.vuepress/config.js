f07f373272c80fdd89e0e1dbdb35ace3d5178f3a
CuteLittleSky <62592731+CuteLittleSky@users.noreply.github.com>
2022-11-09 12:05:33 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  theme:"antdocs",
+  theme:"vuepress-theme-antdocs",
   title: "天空导航",
   description: "欢迎来到天空导航",
   base: "/",
@@ -21,11 +21,15 @@ module.exports = {
     smoothScroll: true,
     nav: require("./config/nav"),
     sidebar: require("./config/sidebar"),
+    sidebar: 'auto'
     search: false,
     lastUpdated: "最后更新",
     repo: "CuteLittleSky/tiankongdaohang",
     docsDir: 'docs',
+    docsBranch: 'master',
+    smoothScroll: true
     editLinks: true,
-    editLinkText: '帮助我们完善天空导航'
+    editLinkText: '帮助我们完善天空导航',
+    }
   },
 };
