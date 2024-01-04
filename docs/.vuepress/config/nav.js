84ce73b3c72761ea421b1accdbdee656b69b6fd4
CuteLittleSky <62592731+CuteLittleSky@users.noreply.github.com>
2024-01-04 21:05:15 +0800

    Update nav.js

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -14,11 +14,19 @@ module.exports = [
     link: "/ys/"
   },
   {
-        text: '粘液科技',
+        text: '粘液科技服主篇',
         ariaLabel: 'Slimefun',
         items: [
           { text: '粘液科技服主食用指南', link: '/slimefun/' },
           { text: '粘液科技附属列表', link: '/slimefun/addons' },
+          { text: '粘液科技Wiki列表', link: '/slimefun/wiki' }
+        ]
+      },
+    {
+        text: '粘液科技玩家篇',
+        ariaLabel: 'sfguide',
+        items: [
+          { text: '粘液科技服主食用指南', link: '/sfguide/' },
           { text: '粘液科技Wiki列表', link: '/slimefun/wiki' }
         ]
       },
