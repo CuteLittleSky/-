f11df11bb67bb73625e3dd0760ea108c993a125c
CuteLittleSky <62592731+CuteLittleSky@users.noreply.github.com>
2024-01-04 21:05:39 +0800

    Update sidebar.js

--- docs/.vuepress/config/sidebar.js
+++ docs/.vuepress/config/sidebar.js
@@ -3,6 +3,7 @@ module.exports = {
   "/pojav/": require("../../pojav/sidebar_contents"),
   "/JEI/": require("../../JEI/sidebar_contents"),
   "/slimefun/": require("../../slimefun/sidebar_contents"),
+  "/sfguide/": require("../../sfguide/sidebar_contents"),
   "/worldedit/": require("../../worldedit/sidebar_contents"),
   "/InventoryProfilesNext/": require("../../InventoryProfilesNext/sidebar_contents"),
   "/SkyBasicPack/": require("../../SkyBasicPack/sidebar_contents"),
